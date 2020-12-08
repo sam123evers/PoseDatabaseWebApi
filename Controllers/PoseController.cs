c844594103c078f3610c0d68a6360d8e24df3d99
Sam Evers <sam123evers@gmail.com>
2020-12-08 15:09:12 -0600

    initial commit

---
+++ Controllers/PoseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PoseDatabaseWebApi.Models;
+
+namespace PoseDatabaseWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PoseController : ControllerBase
+    {
+        Pose pose_hs = new Pose()
+        {
+            PoseName = "Hand Stand"
+        };
+
+        private readonly ILogger<PoseController> _logger;
+
+        public PoseController
+        (
+            //ILogger<PoseController> logger
+        )
+        {
+            //_logger = logger;
+        }
+
+        [HttpGet]
+        public Pose Get()
+        {
+            return pose_hs;
+        }
+    }
+}
---
+++ Models/Pose.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoseDatabaseWebApi.Models
+{
+    public class Pose
+    {
+        public string PoseName { get; set; }
+
+    }
+}
